eb3755bce1e0bfb5d7ac8cd04be92176d8b373df
Phat <phatdroid94@gmail.com>
2022-08-05 18:14:42 +0700

    async await

--- lib/async_synchronized_06072022.dart
+++ lib/async_synchronized_06072022.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+void main() async{
+    try {
+        var value = await Future.delayed(Duration(seconds: 1), () => throw Exception("Lỗi"));
+        print(value);
+    }catch (e) {
+        print(e);
+    }
+
+    // future.then((value) => print(value))
+    //       .catchError((error) => print(error));
 }
