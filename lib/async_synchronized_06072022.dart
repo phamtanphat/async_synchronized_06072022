2fb8adf8e79d83b03d203496e21cf104395a2f1e
Phat <phatdroid94@gmail.com>
2022-08-05 19:18:01 +0700

    object demo2

--- lib/async_synchronized_06072022.dart
+++ lib/async_synchronized_06072022.dart
@@ -1,10 +1,11 @@
-import 'dart:convert';
-
 import 'package:async_synchronized_06072022/demo1.dart';
 import 'package:dio/dio.dart';
 
+import 'demo2.dart';
+
 void main(){
-    handleDemo1();
+    // handleDemo1();
+    // handleDemo2();
 }
 
 void handleDemo1() {
@@ -12,3 +13,9 @@ void handleDemo1() {
         .then((response) => print(Demo1.fromJson(response.data)))
         .catchError((error) => print(error));
 }
+
+// void handleDemo2() {
+//     Dio().get("https://khoapham.vn/KhoaPhamTraining/json/tien/demo2.json")
+//         .then((response) => print(Demo2.fromJson(response.data).toString()))
+//         .catchError((error) => print(error));
+// }
